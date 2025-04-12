18572ccd58a9ab78c7d852a6fd0b1b9ccf813a73
Marco Guzman <167626697+estif56@users.noreply.github.com>
2025-04-11 17:37:17 -0700

    primer commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gcc main.c -o ejemplo-ejecutable
+
+clean:
+	rm -f ejemplo-ejecutable
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("¡Hola, mundo desde la máquina virtual!\n");
+    return 0;
+}
